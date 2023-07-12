0199019ed01c2db91d1fe9db9cdb0700d13fa81e
coffeequickly <jangwonseok.js@gmail.com>
2023-07-12 18:13:33 +0900

    [WIP] queryKey+queryFn 형태로 관리하는 시나리오

--- components/ApiFetcher.tsx
+++ components/ApiFetcher.tsx
@@ -1,15 +1,17 @@
 import React, { ReactNode } from 'react';
-import { QueryObserverResult, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 export function ApiFetcher({
-  query,
+  queryKey,
+  queryFn,
   children,
 }: {
-  query: QueryObserverResult<any, unknown>;
+  queryKey: any;
+  queryFn: () => Promise<AxiosResponse<any>>;
   children: React.ReactNode;
 }) {
-  const { isLoading, data, error } = query;
+  const { isLoading, data, error } = useQuery(queryKey, queryFn);
 
   // TODO: 스켈레톤 추가 필요
   if (isLoading) {
--- components/PostDetailComponent.tsx
+++ components/PostDetailComponent.tsx
@@ -1,17 +1,19 @@
-import React, { Suspense } from 'react';
+import styled from '@emotion/styled';
+import React, { useContext } from 'react';
+
 import axios from 'axios';
-import { ApiErrorBoundary } from '@/components/ApiErrorBoundary';
+
 import { ApiFetcher } from '@/components/ApiFetcher';
-import { useQuery } from '@tanstack/react-query';
+import { ApiErrorBoundary } from '@/components/ApiErrorBoundary';
 import { usePostDetailQuery } from '@/hooks/usePostDetailQuery';
 
 export default function PostDetailComponent({ post_id }: { post_id: string }) {
-  const query = usePostDetailQuery(post_id);
+  const { queryKey, queryFn } = usePostDetailQuery(post_id);
 
   return (
     // TODO: 어떤 주제로 오류를 묶을지 미리 범주를 정의해두고 수집하는 것도 의미 있을듯(Sentry 등 미들웨어 데이터 적재 시 활용)
     <ApiErrorBoundary>
-      <ApiFetcher query={query}>
+      <ApiFetcher queryKey={queryKey} queryFn={queryFn}>
         <PresentationalComponent />
       </ApiFetcher>
     </ApiErrorBoundary>
